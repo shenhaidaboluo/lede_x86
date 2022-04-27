ea6716812eaddf9f6fa25a80872f12d54eca7819
shenhaidaboluo <104182204+shenhaidaboluo@users.noreply.github.com>
2022-04-27 10:26:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git luci-app-v2ray
+git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
