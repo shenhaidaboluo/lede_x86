608a17816a11ccf78e1d80bb980d19f48298d180
shenhaidaboluo <104182204+shenhaidaboluo@users.noreply.github.com>
2022-04-28 01:07:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/openwrt/packages.git packages_github
-cd packages_github && git reset --hard 7aa127e208262e44b81fbbf7fe023b9cfdc6fdf7 && cd ..
-rm -rf feeds/packages/lang/golang && cp -r packages_github/lang/golang feeds/packages/lang/
