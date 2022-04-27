e09bd96047eb1ac3b674a2d46a22891b006ac20e
shenhaidaboluo <104182204+shenhaidaboluo@users.noreply.github.com>
2022-04-28 01:07:56 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,1 +1,3 @@
-#
+git clone https://github.com/openwrt/packages.git packages_github
+cd packages_github && git reset --hard 7aa127e208262e44b81fbbf7fe023b9cfdc6fdf7 && cd ..
+rm -rf feeds/packages/lang/golang && cp -r packages_github/lang/golang feeds/packages/lang/
